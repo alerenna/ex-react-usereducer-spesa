f0545566e7e1cc7eab67eb3772c4aa856e7c4b1e
“alerenna” <alessandrorennafm@gmail.com>
2025-07-02 11:21:42 +0200

    Milestone 1: Add product list to App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,30 @@ import { useState } from 'react'
 
 function App() {
 
+
+  const products = [
+    { name: 'Mela', price: 0.5 },
+    { name: 'Pane', price: 1.2 },
+    { name: 'Latte', price: 1.0 },
+    { name: 'Pasta', price: 0.7 },
+  ]
+
+  return (
+    <>
+      <h1>Lista della spesa</h1>
+      <ul>
+        {products.map((product, i) => (
+          <li key={i}>
+            <p>Prodotto: {product.name} Prezzo: {product.price}€</p>
+          </li>
+        ))}
+      </ul>
+
+    </>
+
+
+
+  )
 }
 
 export default App
